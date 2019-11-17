4c044f6737b976df7c91d916789058b8d5c8ad28
netiabhi <abhineti239@gmail.com>
2019-11-17 09:36:00 +0530

    Testing a simple service with no dependencies

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,24 @@
+import { MessageService } from "./message.service"
+
+describe('Message Service', ()=>{
+    let service: MessageService;
+
+    beforeEach(()=>{
+        service = new MessageService();
+    })
+
+    it('should have no messages to start', ()=>{
+        expect(service.messages.length).toBe(0);
+    })
+
+    it('should add a message when add is called', ()=>{
+        service.add('message1');
+        expect(service.messages.length).toBe(1);
+    })
+
+    it('should clear all message when clear is called', ()=>{
+        service.add('message1');
+        service.clear();
+        expect(service.messages.length).toBe(0);
+    })
+})
